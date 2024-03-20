07fd395f80d1b7178d497f670481d1234af032c5
Miguel Sarmiento <149793793+miguelsarm@users.noreply.github.com>
2024-03-20 00:41:38 -0400

    Simple Landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        principal: "rgb(0, 59, 54)",
+        second: "rgb(210, 187, 160)",
+        third: "rgb(1, 38, 34)",
+        obscure: "rbg(46, 46, 46)",
+      },
     },
   },
   plugins: [],
