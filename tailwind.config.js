9f6e8a6df842ad3a51806346e8f22fc724bb5479
Miguel Sarmiento <149793793+miguelsarm@users.noreply.github.com>
2024-03-23 15:21:01 -0400

    FINISH LANDING PAGE

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       colors: {
         principal: "rgb(0, 59, 54)",
-        second: "rgb(210, 187, 160)",
+        second: "rgb(250,231,148)",
         third: "rgb(1, 38, 34)",
         obscure: "rbg(46, 46, 46)",
       },
